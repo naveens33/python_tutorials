9e6973625c6f6435076b87a0d50b53e65deb5f75
Naveen S <82922481+naveens-tactai@users.noreply.github.com>
2022-04-19 20:15:18 +0530

    Update example1.py

--- strings/example1.py
+++ strings/example1.py
@@ -6,3 +6,10 @@ if word == rev:
     print("Palindorme")
 else:
     print("not Palindrome")
+
+for i in range(len(word)//2):
+    if word[i]!=word[len(word)-i-1]:
+        print("Not Palindorme")
+        break
+else:
+    print("Palindorme")
